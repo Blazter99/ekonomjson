7b5a32fbe79adcf3bab909c581dd8cb5e7ebd1b0
Blazter99 <martin.gunnarsson67@gmail.com>
2022-03-03 14:27:23 +0100

    lite mer variables

--- main.js
+++ main.js
@@ -10,9 +10,20 @@ async function getINFO() {
   const data = await response.json();
   console.log(data);
   const [{konto}] = data;
+  const [{forvaltning}] = data;
+  const [{leverantor}] = data;
+  const [{konterat}] = data;
+
   
-  console.log([{konto}]);
+  console.log(konto);
+  console.log(forvaltning);
+  console.log(leverantor);
+  console.log(konterat);
+
+  document.getElementById("konto").innerHTML = konto;
+
 }
+
 
 
 getINFO();
