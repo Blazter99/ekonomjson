b38fc491a7b342ab082d8d330870ab3dc4cdc358
Blazter99 <martin.gunnarsson67@gmail.com>
2022-03-04 15:18:42 +0100

    ladda om sidan, sidan kraschar

--- main.js
+++ main.js
@@ -9,26 +9,42 @@ async function getINFO() {
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
-  const [{konto}] = data;
-  const [{forvaltning}] = data;
-  const [{leverantor}] = data;
-  const [{konterat}] = data;
+ 
+  function object() {
+    data.forEach(element => {
+      const ul = document.createElement('ul');
+      const faktnr = document.createElement('li');
+      const forvaltning = document.createElement('li');
+      const konterat = document.createElement('li');
+      const konto = document.createElement('li');
+      const leverantor = document.createElement('li');    
+      const orgnr = document.createElement('li');
+
+
+      faktnr.innerHTML = data['fakt.nr'];
+      forvaltning.innerHTML = data.forvaltning;
+      konterat.innerHTML = data.konterat;
+      konto.innerHTML = data.konto;
+      leverantor.innerHTML = data.leverantor;
+      orgnr.innerHTML = data['org.nr'];
+
+      ul.appendChild(faktnr);
+      ul.appendChild(forvaltning);
+      ul.appendChild(konterat);
+      ul.appendChild(konto);
+      ul.appendChild(leverantor);
+      ul.appendChild(orgnr);
+      main.appendChild(ul);
+      
+    });
+  }
   
-  const object = [konto,forvaltning,leverantor,konterat];
-
-  console.log(konto);
-  console.log(forvaltning);
-  console.log(leverantor);
-  console.log(konterat);
-  console.log(object);
-
-  document.getElementById("konto").innerHTML = object;
-
-}
-
-
+  object(data);
+ }
 
 getINFO();
+
+
 
   
 
